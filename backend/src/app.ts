613723abb7a856df42e8c3990e486b24a48df975
Márton Spiteller <spitellermarton@gmail.com>
2026-02-13 14:44:46 +0100

    add: func for graceful shutdown

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,17 +1,37 @@
 import config from "./config/index.js";
 import express from "express";
-import { calculateTotalPrice, Product } from "./cart.js";
+import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import cors from "cors";
+
+import gracefulShutdown from "./utils/gracefulShutdown.js";
+import { initRoutes } from "./routes/index.js";
+
+//import { calculateTotalPrice, Product } from "./cart.js";
 
 const app = express();
 
 const PORT = config.PORT;
 
+app.use(express.json());
+app.use(cookieParser());
+
+/*
 const cart: Product[] = [
   { id: 1, name: "Sushi1", price: 39, quantity: 1 },
   { id: 2, name: "Sushi2", price: 19, quantity: 2 },
   { id: 3, name: "Chopsticks", price: 2, quantity: 1 },
 ];
-
 console.log("Total Price:", calculateTotalPrice(cart));
+*/
 
-app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+initRoutes(app);
+
+const server = app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+
+process.on("SIGINT", async (eventName)=>{
+  await gracefulShutdown(eventName, server);
+});
+process.on("SIGTERM", async (eventName)  => {
+  await gracefulShutdown(eventName, server);
+});
