5bf1dbd81717c114ea65a0cef5cc6a3b811c88be
Márton Spiteller <spitellermarton@gmail.com>
2026-02-14 13:46:37 +0100

    update: object repo to handle main dependencies

--- backend/src/utils/DI.ts
+++ backend/src/utils/DI.ts
@@ -1,50 +1,76 @@
 import { Pool } from "pg";
 import pool from "./database.js";
+import config, { Config } from "../config/index.js";
+import bcrypt from "bcrypt";
 
-// constructing repos
 import UserRepo from "../repositories/user.repository.js";
 import CompanyRepo from "../repositories/company.repository.js";
 
+import AuthService from "../services/auth.service.js";
+import CompanyService from "../services/company.service.js";
+
+import authSchemas, {AuthSchemasInterf} from "../schemas/auth.schemas.js";
+import companySchemas,{CompanySchemasInterf} from "../schemas/company.schemas.js";
+// repos
 interface Repos {
     UserRepo: UserRepo;
     CompanyRepo: CompanyRepo;
 }
 const repos: Repos = {
-    UserRepo: new UserRepo(pool),
-    CompanyRepo: new CompanyRepo(pool),
+    UserRepo: new UserRepo(),
+    CompanyRepo: new CompanyRepo(),
 };
 
-// inject repos into services
-import AuthService from "../services/auth.service.js";
-import CompanyService from "../services/company.service.js";
-
+// services
 interface Services {
     AuthService: AuthService;
     CompanyService: CompanyService;
 }
 const services: Services = {
-    AuthService: new AuthService(repos.UserRepo),
-    CompanyService: new CompanyService(repos.CompanyRepo),
+    AuthService: new AuthService(),
+    CompanyService: new CompanyService(),
+};
+// schemas
+interface Schemas {
+    authSchemas: AuthSchemasInterf;
+    companySchemas: CompanySchemasInterf;
+}
+const schemas: Schemas ={
+    authSchemas: authSchemas,
+    companySchemas: companySchemas
 };
 
 // assembling ObjectRepo
-export interface ObjectRepo {
+export interface ObjectRepoInterf {
     pool: Pool;
     services: Services;
+    repos: Repos;
+    config: Config;
+    bcrypt: object;
+    schemas: Schemas
 }
 
-export class ObjectRepo {
+export class ObjectRepo implements ObjectRepoInterf{
     static instance: ObjectRepo;
-    constructor(pool: Pool, services: Services) {
+    pool!: Pool;
+    services!: Services;
+    repos!: Repos;
+    config!: Config;
+    bcrypt!: object;
+    schemas!: Schemas;
+    constructor(pool: Pool, services: Services, repos: Repos, config: Config, bcrypt: object, schemas: Schemas) {
         if (ObjectRepo.instance) {
             return ObjectRepo.instance;
         }
 
         this.pool = pool;
         this.services = services;
-
+        this.repos = repos;
+        this.config = config;
+        this.bcrypt = bcrypt;
+        this.schemas = schemas;
         ObjectRepo.instance = this;
     }
 }
-const objectRepo = new ObjectRepo(pool, services);
+const objectRepo = new ObjectRepo(pool, services, repos, config, bcrypt, schemas);
 export default objectRepo;
