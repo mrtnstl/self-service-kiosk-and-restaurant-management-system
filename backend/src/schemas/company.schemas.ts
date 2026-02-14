6d0b7cb09df723ab5cb7f8701cd3d2755ad2c085
Márton Spiteller <spitellermarton@gmail.com>
2026-02-14 13:38:21 +0100

    add: company schema

---
+++ backend/src/schemas/company.schemas.ts
@@ -0,0 +1,18 @@
+import * as z from "zod";
+
+export const newCompanySchema = z.object({
+    name: z.string(),
+    logoUrl: z.string()
+});
+
+export interface CompanySchemasInterf {
+    newCompanySchema: z.ZodObject<{
+        name: z.ZodString;
+        logoUrl: z.ZodString;
+    }, z.core.$strip>
+}
+const companySchemas: CompanySchemasInterf = {
+    newCompanySchema: newCompanySchema
+    
+};
+export default companySchemas;
