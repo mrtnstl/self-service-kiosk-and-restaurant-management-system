8afac2dc684b71f23cbe3414e61d4a272a3eed46
Márton Spiteller <spitellermarton@gmail.com>
2026-02-14 16:51:30 +0100

    add: restaurant related classes to object repository

--- backend/src/utils/DI.ts
+++ backend/src/utils/DI.ts
@@ -5,60 +5,78 @@ import bcrypt from "bcrypt";
 
 import UserRepo from "../repositories/user.repository.js";
 import CompanyRepo from "../repositories/company.repository.js";
+import RestaurantRepo from "../repositories/restaurant.repository.js";
 
 import AuthService from "../services/auth.service.js";
 import CompanyService from "../services/company.service.js";
+import RestaurantService from "../services/restaurant.service.js";
 
-import authSchemas, {AuthSchemasInterf} from "../schemas/auth.schemas.js";
-import companySchemas,{CompanySchemasInterf} from "../schemas/company.schemas.js";
+import authSchemas, { AuthSchemasInterf } from "../schemas/auth.schema.js";
+import companySchemas, {
+    CompanySchemasInterf,
+} from "../schemas/company.schema.js";
+import restaurantSchemas, {
+    RestaurantSchemasInterf,
+} from "../schemas/restaurant.schema.js";
+
 // repos
 interface Repos {
     UserRepo: UserRepo;
     CompanyRepo: CompanyRepo;
+    RestaurantRepo: RestaurantRepo;
 }
 const repos: Repos = {
     UserRepo: new UserRepo(),
     CompanyRepo: new CompanyRepo(),
+    RestaurantRepo: new RestaurantRepo(),
 };
 
 // services
 interface Services {
     AuthService: AuthService;
     CompanyService: CompanyService;
+    RestaurantService: RestaurantService;
 }
 const services: Services = {
     AuthService: new AuthService(),
     CompanyService: new CompanyService(),
+    RestaurantService: new RestaurantService(),
 };
 // schemas
 interface Schemas {
     authSchemas: AuthSchemasInterf;
     companySchemas: CompanySchemasInterf;
+    restaurantSchemas: RestaurantSchemasInterf;
 }
-const schemas: Schemas ={
+const schemas: Schemas = {
     authSchemas: authSchemas,
-    companySchemas: companySchemas
+    companySchemas: companySchemas,
+    restaurantSchemas: restaurantSchemas,
 };
 
-// assembling ObjectRepo
-export interface ObjectRepoInterf {
-    pool: Pool;
-    services: Services;
-    repos: Repos;
-    config: Config;
-    bcrypt: object;
-    schemas: Schemas
+// bcrypt
+interface BcryptInterf {
+    genSalt(rounds?: number, minor?: "a" | "b"): Promise<string>;
+    hash(data: string | Buffer, saltOrRounds: string | number): Promise<string>;
 }
 
-export class ObjectRepo implements ObjectRepoInterf{
+// assembling ObjectRepo
+export class ObjectRepo {
     static instance: ObjectRepo;
     pool!: Pool;
     services!: Services;
     repos!: Repos;
     config!: Config;
-    bcrypt!: object;
+    bcrypt!: BcryptInterf;
     schemas!: Schemas;
-    constructor(pool: Pool, services: Services, repos: Repos, config: Config, bcrypt: object, schemas: Schemas) {
+    constructor(
+        pool: Pool,
+        services: Services,
+        repos: Repos,
+        config: Config,
+        bcrypt: BcryptInterf,
+        schemas: Schemas
+    ) {
         if (ObjectRepo.instance) {
             return ObjectRepo.instance;
         }
@@ -72,5 +90,12 @@ export class ObjectRepo implements ObjectRepoInterf{
         ObjectRepo.instance = this;
     }
 }
-const objectRepo = new ObjectRepo(pool, services, repos, config, bcrypt, schemas);
+const objectRepo = new ObjectRepo(
+    pool,
+    services,
+    repos,
+    config,
+    bcrypt,
+    schemas
+);
 export default objectRepo;
