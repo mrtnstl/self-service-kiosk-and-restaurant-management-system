a2981040ce3445b40eb4a19f1859cde8f7ab5d1d
Márton Spiteller <spitellermarton@gmail.com>
2026-02-14 13:43:59 +0100

    add: auth schemas export for dependency injection

--- backend/src/schemas/auth.schemas.ts
+++ backend/src/schemas/auth.schemas.ts
@@ -4,7 +4,7 @@ import constants from "../constants/index.js";
 export const nonManagerUserSchema = z.object({
     name: z.string(),
     password: z.string().min(constants.PW_MIN_L),
-    role: z.string(),
+    roleId: z.number(),
     restaurantId: z.uuid(),
     companyId: z.uuid(),
 });
@@ -15,3 +15,24 @@ export const managerUserSchema = z.object({
     password: z.string().min(constants.PW_MIN_L), 
     companyId: z.string()
 });
+
+export interface AuthSchemasInterf {
+    nonManagerUserSchema: z.ZodObject<{
+        name: z.ZodString;
+        password: z.ZodString;
+        roleId: z.ZodNumber;
+        restaurantId: z.ZodUUID;
+        companyId: z.ZodUUID;
+    }, z.core.$strip>,
+        managerUserSchema: z.ZodObject<{
+        email: z.ZodString;
+        name: z.ZodString;
+        password: z.ZodString;
+        companyId: z.ZodString;
+    }, z.core.$strip>
+}
+const authSchemas: AuthSchemasInterf = {
+    nonManagerUserSchema,
+    managerUserSchema
+};
+export default authSchemas;
