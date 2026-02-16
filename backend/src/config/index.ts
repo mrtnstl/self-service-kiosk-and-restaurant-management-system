c5a8f34783a9a8384881b795aa3c34bff195ad01
Márton Spiteller <spitellermarton@gmail.com>
2026-02-16 09:55:22 +0100

    update: type casting values

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -1,16 +1,19 @@
 export interface Config {
-    PORT: string,
-    NODE_ENV: string,
-    FRONTEND_URL: string,
-    DATABASE_CONNECTION_STRING: string,
-    CORS_OPTIONS: {origin: string, credentials: boolean}
+    PORT: string;
+    NODE_ENV: string;
+    BASE_URL: string;
+    FRONTEND_URL: string;
+    DATABASE_CONNECTION_STRING: string;
+    CORS_OPTIONS: { origin: string; credentials: boolean };
 }
 const config: Config = {
-    PORT: process.env.PORT || "",
-    NODE_ENV: process.env.NODE_ENV || "",
-    FRONTEND_URL: process.env.FRONTEND_URL || "",
-    DATABASE_CONNECTION_STRING: process.env.DATABASE_CONNECTION_STRING || "",
-    CORS_OPTIONS: {origin: process.env.FRONTEND_URL || "", credentials: true}
+    PORT: process.env.PORT as string,
+    NODE_ENV: process.env.NODE_ENV as string,
+    BASE_URL: process.env.BASE_URL as string,
+    FRONTEND_URL: process.env.FRONTEND_URL as string,
+    DATABASE_CONNECTION_STRING: process.env
+        .DATABASE_CONNECTION_STRING as string,
+    CORS_OPTIONS: { origin: process.env.FRONTEND_URL || "", credentials: true },
 };
 
 export default config;
