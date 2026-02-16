1bedb0f9cd376630e6fdc18de199c2b06cbd0f78
Márton Spiteller <spitellermarton@gmail.com>
2026-02-16 10:08:51 +0100

    update: error obj creation

--- backend/src/middleware/wildcardRouteHandlerMW.ts
+++ backend/src/middleware/wildcardRouteHandlerMW.ts
@@ -1,7 +1,11 @@
 import { RequestHandler } from "express";
+import { NotFoundError } from "../errors/index.js";
 
 export default (): RequestHandler => {
     return async (_req, res, _next) => {
-        return res.status(404).json({ message: "resource not found." });
+        const notFoundError = new NotFoundError();
+        return res
+            .status(notFoundError.statusCode)
+            .json({ message: notFoundError.message });
     };
 };
